ff1715c5c3a258170ca7fd773a58a552424c1cc4
Yury Litvinov <litvinov.yury@gmail.com>
2022-11-04 21:56:29 -0700

    simulate api call

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from "dotenv";
-import {ethers} from "ethers";
+import {ethers, Transaction} from "ethers";
+import axios from "axios";
 
 require('console-stamp')(console);
 
@@ -24,6 +25,39 @@ app.post('/', async (req, res) => {
 
 async function dryRunTransaction(serializedTransaction) {
     let deserializedTx = await ethers.utils.parseTransaction(serializedTransaction);
-
+    let tenderlyData = await simulateWithTenderly(deserializedTx);
+    console.log(tenderlyData)
     return {}
+}
+
+
+async function simulateWithTenderly(deserializedTx: Transaction) {
+    let tenderlyBody = {
+        network_id: deserializedTx.chainId,
+        from: deserializedTx.from,
+        to: deserializedTx.to,
+        input: deserializedTx.data,
+        gas: 200000,
+        gas_price: 100,
+        value: deserializedTx.value.toString(),
+    };
+    console.log(JSON.stringify(tenderlyBody))
+    const tenderlyUrl =
+        "https://api.tenderly.co/api/v1/account/" +
+        process.env.TENDERLY_USER +
+        "/project/" +
+        process.env.TENDERLY_PROJECT +
+        "/simulate";
+
+    let tenderlyResponse = await axios.post(
+        tenderlyUrl, tenderlyBody, {
+            headers: {
+                'content-type': 'application/JSON',
+                "X-Access-Key": process.env.TENDERLY_ACCESS_KEY,
+            },
+        }
+    );
+    let tenderlyData = tenderlyResponse.data;
+    console.log(JSON.stringify(tenderlyData))
+    return tenderlyData;
 }
