dc0b414139ea66b11242ab4993cd1e4f4b05688e
Yury Litvinov <litvinov.yury@gmail.com>
2022-11-04 21:26:36 -0700

    transaction parsing

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,29 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import * as dotenv from "dotenv";
+import {ethers} from "ethers";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+require('console-stamp')(console);
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+const port = 8000;
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+app.post('/', async (req, res) => {
+    let signedTransaction = req.body.transaction;
+    let result = await dryRunTransaction(signedTransaction);
+    res.send(result);
+});
+
+async function dryRunTransaction(serializedTransaction) {
+    let deserializedTx = await ethers.utils.parseTransaction(serializedTransaction);
+
+    return {}
+}
