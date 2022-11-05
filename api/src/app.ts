fd52cdf0ee5a4ca910e8d5df1c6b1db6e10afc62
Yury Litvinov <litvinov.yury@gmail.com>
2022-11-04 22:38:10 -0700

    building response

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import * as dotenv from "dotenv";
-import {ethers, Transaction} from "ethers";
+import {BigNumber, ethers, Transaction} from "ethers";
 import axios from "axios";
+import {legos} from "@studydefi/money-legos";
 
 require('console-stamp')(console);
 
@@ -26,10 +27,49 @@ app.post('/', async (req, res) => {
 async function dryRunTransaction(serializedTransaction) {
     let deserializedTx = await ethers.utils.parseTransaction(serializedTransaction);
     let tenderlyData = await simulateWithTenderly(deserializedTx);
-    console.log(tenderlyData)
-    return {}
-}
 
+    if (!tenderlyData.transaction.status) {
+        return {"Transaction failed": tenderlyData.transaction.error_message}
+    }
+
+    const contractsMap = {}
+    for (const contract of tenderlyData.contracts) {
+        contractsMap[contract.address] = contract
+    }
+
+    let result = {}
+    let callTraces = tenderlyData.transaction.call_trace;
+    for (let i = 0; i < callTraces.length; i++) {
+        let callTrace = callTraces[i];
+        const contract = contractsMap[callTrace["to"]]
+        if (contract == null) {
+            console.log({"Unknown contract": callTrace["to"]})
+            continue
+        }
+
+        if (contract.standards && contract.standards.includes("erc20")) {
+            console.log(callTrace)
+
+            try {
+                const iface = new ethers.utils.Interface(legos.erc20.abi);
+                const decodedArgs = iface.decodeFunctionData(callTrace.input.slice(0, 10), callTrace.input)
+                const functionName = iface.getFunction(callTrace.input.slice(0, 10)).name
+                console.log(functionName)
+                console.log(decodedArgs)
+                if (functionName == "transfer") {
+                    let toAddress = decodedArgs[0]
+                    let amount = (decodedArgs[1] as BigNumber).toString()
+                    let title = "Transfer " + amount + " " + contract["token_data"]["symbol"] + " to " + toAddress;
+                    let description = "OK";
+                    result[title] = description
+                }
+            } catch (e) {
+                console.log("Failed to decode callTrace for " + contract.address)
+            }
+        }
+    }
+    return result
+}
 
 async function simulateWithTenderly(deserializedTx: Transaction) {
     let tenderlyBody = {
@@ -60,29 +100,5 @@ async function simulateWithTenderly(deserializedTx: Transaction) {
     let tenderlyData = tenderlyResponse.data;
     console.log(JSON.stringify(tenderlyData))
 
-    if (!tenderlyData.transaction.status) {
-        return {"Transaction failed": tenderlyData.transaction.error_message}
-    }
-
-    const contractsMap = {}
-    for (const contract of tenderlyData.contracts) {
-        contractsMap[contract.address] = contract
-    }
-
-    let callTraces = tenderlyData.transaction.call_trace;
-    for (let i = 0; i < callTraces.length; i++) {
-        let callTrace = callTraces[i];
-        const contract = contractsMap[callTrace["to"]]
-        if (contract == null) {
-            console.log({"Unknown contract": callTrace["to"]})
-            continue
-        }
-
-        if (contract.standards && contract.standards.includes("erc20")) {
-            console.log(callTrace)
-
-
-        }
-    }
-    return tenderlyData;
+    return tenderlyData
 }
